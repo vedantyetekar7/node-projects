2499bc2b68a2a93e25c1b8af9a60b5516f0f6a9f
vedant <ursvedantyetekar@gmail.com>
2022-07-07 17:25:13 +0530

    done with node-basics

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 // declaring and printing variables
 const a = 10;
 const b = 20;
@@ -96,7 +95,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () =>
-console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
 );
 
 // creating a http server using node (another way)
@@ -105,14 +104,14 @@ const hostname = "127.0.0.1";
 const port = 5000;
 
 http
-.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("<h1>Hello</h1>");
-  res.end();
-})
-.listen(port, hostname, () =>
-console.log(`Server running at http://${hostname}:${port}/`)
-);
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("<h1>Hello</h1>");
+    res.end();
+  })
+  .listen(port, hostname, () =>
+    console.log(`Server running at http://${hostname}:${port}/`)
+  );
 
 // making a http get request
 const http = require("http");
@@ -148,14 +147,13 @@ const options = {
 const req = https.request(options, (res) => {
   let data = "";
   console.log("Status Code: ", res.statusCode);
-  
+
   res.on("data", (chunk) => (data += chunk));
   res.on("end", () => console.log(JSON.parse(data)));
 });
 
 req.write(data);
 req.end();
-
 
 // making a http post request using axios
 const axios = require("axios");
@@ -167,12 +165,12 @@ const data = JSON.stringify({
 });
 
 axios
-.post("https://reqres.in/api/users", data)
-.then((res) => {
-  console.log(`Status Code: ${res.status}`);
-  console.log(`Body: ${JSON.stringify(res.data)}`);
-})
-.catch((error) => console.log(error));
+  .post("https://reqres.in/api/users", data)
+  .then((res) => {
+    console.log(`Status Code: ${res.status}`);
+    console.log(`Body: ${JSON.stringify(res.data)}`);
+  })
+  .catch((error) => console.log(error));
 
 // working with files
 const fs = require("fs");
@@ -236,7 +234,7 @@ fs.unlink("test.txt", (error) => {
     console.log(error);
     return;
   }
-  
+
   console.log("File Removed.");
 });
 
@@ -245,6 +243,181 @@ const fs = require("fs");
 
 fs.unlinkSync("test.txt");
 
-*/
+// routing
+const http = require("http");
 
-// 02:11:00
+const index = (req, res) => {
+  res.writeHead(200);
+  res.write("<h1>Welcome to my profile.</h1>");
+  res.end();
+};
+
+const about = (req, res) => {
+  res.writeHead(200);
+  res.write("<h1>Hello, I'm Vedant Yetekar.</h1>");
+  res.end();
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url == "/") {
+      return index(req, res);
+    }
+
+    if (req.url == "/about") {
+      return about(req, res);
+    }
+  })
+  .listen(5000);
+
+// refactored the routing code
+
+const http = require("http");
+
+const routes = {
+  "/": (req, res) => {
+    res.writeHead(200);
+    res.write("<h1>Welcome to the homepage.</h1>");
+    res.end();
+  },
+
+  "/about": (req, res) => {
+    res.writeHead(200);
+    res.write("<h1>Hello, I'm Vedant Yetekar.</h1>");
+    res.end();
+  },
+};
+
+http
+  .createServer((req, res) => {
+    if (req.url in routes) {
+      return routes[req.url](req, res);
+    }
+  })
+  .listen(process.env.PORT || 5000);
+
+// path module
+const path = require("path");
+
+const lastName = path.basename("test.json");
+const dirName = path.dirname("node-projects/test.json");
+const absPath = path.isAbsolute("C:/Users/Vedan/Desktop/node-projects");
+const dirPath = path.join(__dirname, "test.json");
+const parsedPath = path.parse(dirPath);
+const resolvedPath = path.resolve("test.json");
+
+console.log(lastName);
+console.log(dirName);
+console.log(absPath);
+console.log(dirPath);
+console.log(parsedPath);
+console.log(resolvedPath);
+
+// event module
+const http = require("http");
+const events = require("events");
+const event = new events.EventEmitter();
+
+const routes = {
+  "/": (req, res) => {
+    res.writeHead(200);
+    res.write("<h1>HOME PAGE</h1>");
+    res.end();
+  },
+  "/about": (req, res) => {
+    res.writeHead(200);
+    res.write("<h1>ABOUT PAGE</h1>");
+    res.end();
+  },
+};
+
+let cnt = 0;
+
+event.on("my_event", (cnt) => {
+  console.log(`Event Called: ${cnt}`);
+});
+
+event.on("my_event", (cnt) => {
+  console.log(`Event Called: ${cnt}`);
+});
+
+event.once("event_once", () => console.log("EVENT ONCE FIRED."));
+
+event.off("my_event", (cnt) => {
+  console.log(`Event Called: ${cnt}`);
+});
+
+http
+  .createServer((req, res) => {
+    if (req.url in routes) return routes[req.url](req, res);
+
+    cnt++;
+    event.emit("my_event", cnt);
+    event.emit("event_once");
+  })
+  .listen(process.env.PORT || 5000);
+
+// stream: used when files are too big.
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("test.json", (error, file) => {
+    const stream = fs.createReadStream("test.json");
+    stream.pipe(res);
+  });
+});
+
+server.listen(5000, () =>
+  console.log(`Server running at http://localhost:5000`)
+);
+
+// buffer: used to deal with binary data (related to streams).
+
+const buff1 = Buffer.from("hey there!");
+const buff2 = Buffer.alloc(4);
+buff2.write("Hey!");
+
+console.log(buff1);
+console.log(buff1.toString());
+console.log(buff1[0]);
+console.log(buff1[1]);
+console.log(buff2);
+console.log(buff2.toString());
+buff1[0] = 111; // o
+console.log(buff1.toString());
+
+// exception handling
+// throw new Error("An error encountered.");
+
+// try-catch block
+try {
+  const tryVar = 0;
+  tryVar = 10;
+  console.log("I'm Try Block");
+} catch (error) {
+  console.log(error.message);
+}
+
+// handling error in promises
+doSomething1()
+  .then(() => {
+    return doSomething2().catch((error) => {
+      throw error;
+    });
+  })
+  .then(() => {
+    return doSomething2().catch((error) => {
+      throw error;
+    });
+  })
+  .catch((error) => console.log(error));
+
+// handling errors in async await functions
+const handleError = async () => {
+  try {
+    await doSomething1();
+  } catch (error) {
+    console.log(error);
+  }
+};
