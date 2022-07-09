160fcfe909c272d3159f2cad929e5bc25bd185ac
vedant <ursvedantyetekar@gmail.com>
2022-07-09 16:25:44 +0530

    update:

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const PORT = process.env.PORT || 5000;
 const publicPath = path.join(__dirname, "public");
 const assetsPath = path.join(publicPath, "assets");
 
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/static", express.static(publicPath));
 app.use("/assets", express.static(assetsPath));
