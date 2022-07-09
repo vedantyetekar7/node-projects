d0432653693fc49f2083dbad5c024cd786eabe93
vedant <ursvedantyetekar@gmail.com>
2022-07-09 18:58:48 +0530

    done with node-login-app

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+const router = require("./router.js");
 
 const PORT = process.env.PORT || 5000;
 const publicPath = path.join(__dirname, "public");
@@ -15,13 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/static", express.static(publicPath));
 app.use("/assets", express.static(assetsPath));
+app.use(session({ secret: uuidv4(), resave: false, saveUninitialized: true }));
+app.use("/route", router);
 
 const baseData = {
   title: "Login System",
+  logout: "",
 };
 
 app.get("/", (req, res) => {
-  res.render("base", { baseData });
+  res.render("base", { ...baseData });
 });
 
 app.listen(PORT, () =>
